623b07e86827bcabf42565f29f67e8c1aa9ea059
davidxcheng <davidxcheng@gmail.com>
2012-10-15 01:50:45 +0200

    Had to base64 encode font since web server wouldn't serve *.woff-files.

--- js/slnblvd.js
+++ js/slnblvd.js
@@ -4,25 +4,11 @@
 */
 
 $(document).ready(function() {
-	$('div#offset1-layer').html($('div#top-layer').html());
-	$('div#offset2-layer').html($('div#top-layer').html());
-	
-	// Set the height of the div that contains the logo.
-	$('#logo').height($('#top-layer').height());
-	
 	// Link to startpage.
 	$('.logo').live('click', function(e){
 			e.preventDefault();
-			document.location = 'http://www.solutionblvd.se';
+			document.location = '/index.html';
 		});
-	
-	$(window).bind('resize', function(){
-		// Re-set the height of the div that contains the logo.
-		$('#logo').height($('#top-layer').height());
-	});
 
 	$('a.email').nospam({ replaceText: true, filterLevel: 'low' });
-	
-	$('div#icon-offset1-layer').html($('div#icon-top-layer').html());
-	$('div#icon-offset2-layer').html($('div#icon-top-layer').html());
 });
