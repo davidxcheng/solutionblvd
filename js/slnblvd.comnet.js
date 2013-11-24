103abacfa9c37c3429f48b1553b22f57e524410e
David Cheng <davidxcheng@gmail.com>
2013-11-24 02:42:23 +0100

    display info about layer on mouse over (needs styling)

--- js/slnblvd.comnet.js
+++ js/slnblvd.comnet.js
@@ -21,4 +21,15 @@
 			'unit of data': 'bit'
 		}
 	};
+
+	var $ = (function(id) {	return document.getElementById(id);	}),
+		protocolsEl = $('protocols'),
+		unitEl = $('unit');
+
+	$('layers').addEventListener('mouseover', function(e) {
+		if (e.target.classList && e.target.classList[0] == "layer") {
+			protocolsEl.innerText = layers[e.target.id].protocols.join(", ");
+			unitEl.innerText = layers[e.target.id]["unit of data"];
+		}
+	});
 })();
